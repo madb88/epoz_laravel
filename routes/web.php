d7cd7621df9d8db414b31a44a2fe646f0ecf2977
madb88 <kaminskiqba@gmail.com>
2017-01-19 19:48:47 +0100

    Ammendments to routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
 //Charts routes
-Route::get('charts', 'ChartsController@index');
-Route::get('charts/create', 'ChartsController@create');
-Route::get('charts/{id}', 'ChartsController@show');
-Route::post('charts', 'ChartsController@store');
+
+Route::resource('charts', 'ChartsController');
